499c938d79e9a46fe502b65f97a70961d1513cd2
Slowist-Lee <3230103034@zju.edu.cn>
2025-08-26 18:30:33 +0800

    update gitignore

--- site/Math/MathModel/add.py
+++
@@ -1,6 +0,0 @@
-with open('D:\\MyRepository\\notebook-publish\\notebook\\docs\\Math\\MathModel\\note\\graph\\graph.md', 'r', encoding='utf-8') as file:
-    lines = file.readlines()
-
-with open('D:\\MyRepository\\notebook-publish\\notebook\\docs\\Math\\MathModel\\note\\graph\\graph.md', 'w', encoding='utf-8') as file:
-    for line in lines:
-        file.write(line.rstrip('\n') + '  \n')
